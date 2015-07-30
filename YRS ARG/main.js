dc33a57cc9c1a7433412f46dd40b106127acbc11
TheTruthOfTheRedPill <kalyan.sudra@gmail.com>
2015-07-30 14:34:57 +0100

    Red-Pill v6

--- YRS ARG/main.js
+++ YRS ARG/main.js
@@ -1,22 +1,19 @@
-var map = L.map(this.element.dom).setView([47.36865, 8.539183], 13);
-
-var marker = L.marker([51.5, -0.09]).addTo(map);
-
-var circle = L.circle([51.508, -0.11], 500, {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5
-}).addTo(map);
-
-var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(map);
+var map = new L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
-    id: 'your.mapbox.project.id',
-    accessToken: 'your.mapbox.public.access.token'
+    id: 'kalzone.599fe992',
+    accessToken: 'pk.eyJ1Ijoia2Fsem9uZSIsImEiOiJiZWY2YWQ2YTJmMTA1MmQwYmMxYmRlMjhlZTc3YzU5MCJ9.CM4Wm4F8t-gd1MtUAwxTtA'
 }).addTo(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString() + " Please post this to the forum if there is a lead at this location.")
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
